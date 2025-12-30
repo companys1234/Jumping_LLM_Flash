ad6f4bdbe2dae8c43af982bf131676a7024c763d
poco40 <128311098+companys1234@users.noreply.github.com>
2025-12-30 14:04:19 +0500

    Update __init__.py

--- scr/architecture/__init__.py
+++ scr/architecture/__init__.py
@@ -2,5 +2,7 @@ from .Swiglu import SwigLU
 from .RMSNorm import RMSNorm
 from .GMQA_with_KV import GMQA_with_KV
 from .Feed_Foward_Network import Feed_Forward_Network
+from .rope import rope
 
-__all__ = ['SwigLU', 'RMSNorm', 'GMQA_with_KV', 'Feed_Forward_Network']
+__all__ = ['SwigLU', 'RMSNorm', 'GMQA_with_KV', 'Feed_Forward_Network','rope']
+
