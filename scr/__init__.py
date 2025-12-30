a1eaea468061d135af1b814933bfac523a91bed0
poco40 <128311098+companys1234@users.noreply.github.com>
2025-12-30 13:35:00 +0500

    Update __init__.py
    починили импорт

--- scr/__init__.py
+++ scr/__init__.py
@@ -1,5 +1,5 @@
-from architecture import SwigLU, RMSNorm, GMQA_with_KV, Feed_Forward_Network
-from inference import get_activation, print_model_info, get_linear_activation, quantize_linear_layers
-from preprocessing import SimpleTokenizer
+from .architecture import SwigLU, RMSNorm, GMQA_with_KV, Feed_Forward_Network
+from .inference import get_activation, print_model_info, get_linear_activation, quantize_linear_layers
+from .preprocessing import SimpleTokenizer
 
 __all__ = ['get_activation', 'print_model_info', 'get_linear_activation', 'quantize_linear_layers', 'SwigLU', 'RMSNorm', 'GMQA_with_KV', 'Feed_Forward_Network', 'SimpleTokenizer']
